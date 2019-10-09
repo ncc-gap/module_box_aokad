dccc8cc629d1423f7c293c69d8d5f048b1b8944b
Ai Okada <aokada@ncc.go.jp>
2019-10-09 16:21:58 +0900

    add task files

---
+++ 20191009-featureCounts/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+OUTPUT_PREF=${OUTPUT_DIR}/${SAMPLE}
+mkdir -p ${OUTPUT_DIR}
+
+featureCounts -T 4 -p -a ${GTF} -O -B -C -o ${OUTPUT_PREF}.txt ${INPUT}
+python /tools/simple_exp/proc_fc.py ${OUTPUT_PREF}.txt ${OUTPUT_PREF}.txt.summary ${GTF} > ${OUTPUT_PREF}.txt.fpkm
